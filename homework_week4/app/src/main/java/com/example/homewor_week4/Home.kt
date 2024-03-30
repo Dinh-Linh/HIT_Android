2b5ccca61a621623a579515df4b87dbbda9133ee
Dinh-Linh <140419823+Dinh-Linh@users.noreply.github.com>
2024-03-30 22:16:07 +0700

    commited

--- homework_week4/app/src/main/java/com/example/homewor_week4/Home.kt
+++ homework_week4/app/src/main/java/com/example/homewor_week4/Home.kt
@@ -10,17 +10,27 @@ class Home : AppCompatActivity() {
     private lateinit var name: String
     private lateinit var pass: String
     private val list = mutableListOf<User>()
+    private lateinit var userAdapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         name = intent.getStringExtra("username").toString()
         pass = intent.getStringExtra("pass").toString()
-        list.add(User(name, pass, R.drawable.user_alt_light))
-        val userAdapter = UserAdapter(list)
+
+        //list.add(User(name, pass, R.drawable.user_alt_light))
+        userAdapter = UserAdapter(list)
         binding.recylerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.recylerView.adapter = userAdapter
+        addUserToList(name, pass)
+    }
+
+    private fun addUserToList(username: String, password: String) {
+        val newUser = User(username, password, R.drawable.user_alt_light)
+        list.add(newUser)
+        userAdapter.notifyItemInserted(list.size - 1)
     }
 }
--- homework_week4/app/src/main/java/com/example/homewor_week4/LoginForm.kt
+++ homework_week4/app/src/main/java/com/example/homewor_week4/LoginForm.kt
@@ -7,24 +7,32 @@ import com.example.homewor_week4.databinding.ActivityLoginBinding
 
 class LoginForm : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val list = mutableListOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val intent = Intent(this, Home::class.java)
+        //val intent = Intent(this, Home::class.java)
         binding.login.setOnClickListener{
             val username = binding.username.text.toString()
             val pass = binding.password.text.toString()
-            intent.putExtra("username", username)
-            intent.putExtra("pass", pass)
-            startActivity(intent)
+            //intent.putExtra("username", username)
+            //intent.putExtra("pass", pass)
+            //list.add(User(username, pass, R.layout.itemuser))
+            navigateToHome(username, pass)
         }
 
         binding.signUp.setOnClickListener{
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+    }
+    private fun navigateToHome(username: String, password: String) {
+        val intent = Intent(this, Home::class.java)
+        intent.putExtra("username", username)
+        intent.putExtra("password", password)
+        startActivity(intent)
     }
 }
 
